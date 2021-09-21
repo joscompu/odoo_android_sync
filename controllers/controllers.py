726de360a1b192bd1027db291d8452a3ca172e43
Jose Calderon <joscompu@gmail.com>
2021-09-21 01:04:52 -0300

    Se corriges codigo.

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -6,19 +6,18 @@ import json
 
 class OdooAndroidSync(http.Controller):
 
-        @http.route('/attendance/v1/hours/<user_id>/<day_week>', auth='user', methods=['GET'])
-        def get_attendance_hours(self, user_id, day_week, **kw):
-                try:
-                    calendar_id = http.request.env['hr.employee'].sudo().search([('user_id', '=', int(user_id))]).resourse_calendar_id.id
-                    hours = http.request.env['resource.calendar.attendance'].sudo().search_read([('calendar_id', '=', int(calendar_id)), ('day_week', '=', )],
-                                       ['hour_from', 'hour_to'])
-                return build_response(hours)
-            except Exception as e:
-                return build_response({'err': str(e)})
+    @http.route('/attendance/v1/hours/<user_id>/<day_week>', auth='user', methods=['GET'])
+    def get_attendance_hours(self, user_id, day_week, **kw):
+        try:
+            calendar_id = http.request.env['hr.employee'].sudo().search(
+                [('user_id', '=', int(user_id))]).resourse_calendar_id.id
+            hours = http.request.env['resource.calendar.attendance'].sudo().search_read(
+                [('calendar_id', '=', int(calendar_id)), ('day_week', '=',)],
+                ['hour_from', 'hour_to'])
+            return build_response(hours)
+        except Exception as e:
+            return build_response({'err': str(e)})
 
-        def build_response(self, entity):
-                response = json.dumps(entity, ensure_ascii=False).encode('utf8')
-                return Response(response, content_type='application/json;charset=utf-8', status=200)
-
-
-
+    def build_response(self, entity):
+        response = json.dumps(entity, ensure_ascii=False).encode('utf8')
+        return Response(response, content_type='application/json;charset=utf-8', status=200)
